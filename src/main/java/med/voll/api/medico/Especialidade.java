04596a68a207f194fcfe3ace5e15cf29896a96aa
devbandeira <dev.bandeira@outlook.com>
2023-09-21 18:18:24 -0300

    DTO com JAVA RECORD

---
+++ src/main/java/med/voll/api/medico/Especialidade.java
@@ -0,0 +1,10 @@
+package med.voll.api.medico;
+
+public enum Especialidade {
+    /*lembrar de avisar ao front quando for trazer os dados para a nossa API JAVA que os dados tem que ser tratados
+    * pois nosso enum é em letras maiusculas*/
+    ORTOPEDIA,
+    CARDIOLOGIA,
+    GINECOLOGIA,
+    DERMATOLOGIA;
+}
