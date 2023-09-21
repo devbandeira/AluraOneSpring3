faeabcf93fd2994a8bf84b2e09016b91c7ae15f1
devbandeira <dev.bandeira@outlook.com>
2023-09-21 18:56:49 -0300

    Adicionando dependencias - application.properties

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -3,7 +3,6 @@ package med.voll.api.medico;
 import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.Especialidade;
 
-/*Classe RECORDS ja cria todos getters/setters e construtores automaticamente e uma classe imutavel*/
 public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade,
                                   Endereco endereco) {
 }
--- src/main/java/med/voll/api/medico/Especialidade.java
+++ src/main/java/med/voll/api/medico/Especialidade.java
@@ -1,8 +1,6 @@
 package med.voll.api.medico;
 
 public enum Especialidade {
-    /*lembrar de avisar ao front quando for trazer os dados para a nossa API JAVA que os dados tem que ser tratados
-    * pois nosso enum é em letras maiusculas*/
     ORTOPEDIA,
     CARDIOLOGIA,
     GINECOLOGIA,
