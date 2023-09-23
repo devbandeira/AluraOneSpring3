c335f4ece3f17f083c50fb6409b43479e2b8f0cf
devbandeira <dev.bandeira@outlook.com>
2023-09-23 12:13:57 -0300

    Exercicios para LISTA PACIENTE usando PAGEABLE

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -5,6 +5,8 @@ import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
+import med.voll.api.paciente.DadosListagemPaciente;
+import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,21 +21,22 @@ public class MedicoController {
     @Autowired
     private MedicoRepository repository;
 
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
     @PostMapping
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
     }
 
-    /*Posso enviar por URL os parametros de SIZE&PAGE&SORT, mas posso mudar o padrão do PAGEABLE aqui
-    * por padrão ele tem size 20, etc. Se for informado o SIZE na url esse padrão, se comporta conforme url mandar.
-    * Padrão Spring: Size = 20, PAGE = 0, SORT conforme foi inserido no db*/
     @GetMapping
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, page = 0, sort = {"nome"}) Pageable paginacao){
         return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listarPaciente(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+        return pacienteRepository.findAll(paginacao).map(DadosListagemPaciente::new);
+    }
 }
-/*Posso mudar os nomes SIZE - PAGE - etc para portugues no application.properties*/
-/*se eu abrir o terminal aqui no intelij, vejo que não mostra qual SQL foi executado para essa query. Posso ativar
-mudando a configuração no aplication.properties
-* */
