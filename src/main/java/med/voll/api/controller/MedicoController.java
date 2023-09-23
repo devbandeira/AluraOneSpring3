3eb355645ae27280d36a37ac8ce068a119fd1410
devbandeira <dev.bandeira@outlook.com>
2023-09-22 23:56:42 -0300

    Anotação MINHA sobre o costrutor na ENTITY Medico recebendo dados do meu DTO

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -20,22 +20,16 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){/*OBS. Minha: Como aqui para salvar ele recebe
+    DadosCadastroMedico que é o record DTO, mas eu tenho que salvar minha ENTITY(ENTIDADE) Medico, crio um construtor
+    na ENTITY Medico que quando for chamada "new Medico" é preechida pelos dados do DTO, conforme cada atributo que eu
+    preciso*/
         repository.save(new Medico(dados));
     }
 
     @GetMapping
-    public List<DadosListagemMedico> listar(){/*Se eu retornar um Medico, é um problema grande, como outros problemas por se trabalhar
-    com entidade JPA. Nesse caso ele vai retornar todas as informações de médico, mas na nossa regra de negocio, queremos
-    somente algumas informações, então vou gerar um novo DTO para isso, onde vamos devolver dados da API.
-    Antes era<Medico> agora vai ser <DadosListagemMedico>*/
-        return repository.findAll().stream().map(DadosListagemMedico::new).toList();/*findAll retorna o tipo MEDICO, a entidade JPA. Então da erro,Entao vamos ter que converter
-        de MEDICO para DadosListagemMedico, usando o -> .stream().map(DadosListagemMedico::new)
-        Dessa forma (DadosListagemMedico::new) chamo o construtor do nosso DTO, mas vai reclamar, pq dentro do DadosListagemMedico
-        não tem um construtor que recebe um objeto do tipo MEDICO, então precisamos criar esse construtor no DTO record.
-
-.toList() No final uso essa função para transformar para uma lista
-        */
+    public List<DadosListagemMedico> listar(){
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 
 }
--- src/main/java/med/voll/api/medico/DadosListagemMedico.java
+++ src/main/java/med/voll/api/medico/DadosListagemMedico.java
@@ -1,6 +1,6 @@
 package med.voll.api.medico;
 
-/*Aqui no DTO ponho somente os campos que quero devolver para o frontend*/
+
 public record DadosListagemMedico(
         String nome,
         String email,
@@ -8,10 +8,7 @@ public record DadosListagemMedico(
         Especialidade especialidade
 ) {
     public DadosListagemMedico(Medico medico){
-     /*Agora temos que chamar o proprio construtor do RECORD.
-     * Podemos ter varios construtores aqui nesse RECORD mas temos que chamar o construtor principal*/
+
         this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
     }
-    /*Feito esse nosso construtor que a partir de Medico ele preenche as informações do construtor desse record aqui
-    * DadosListagemMedico*/
 }
