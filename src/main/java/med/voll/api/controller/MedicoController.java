5957464e61d3342e77384114bedf202ef1e137b5
devbandeira <dev.bandeira@outlook.com>
2023-09-23 11:35:02 -0300

    Fazendo Paginação com SPRING

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,10 +6,10 @@ import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -25,12 +25,13 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> listar(){
-        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> listar(Pageable paginacao){/*Passando a class Pageable do Spring e vamos mudar
+    o tipo List<> para uma classe do proprio Springboot chamada Page*/
+        /*No repository existe uma sobrecarga do findAll que recebe um Pageble como parametro*/
+        /*Vamos ter que tirar o stream, porque o findall devolve um Page e o PAGE ja tem o método MAP diretamente e remover
+        * o toList() no final da fn
+        * Antigo -> return repository.findAll(paginacao).stream().map(DadosListagemMedico::new).toList();
+        * chamando o MAP ele já faz a conversão e devolve um PAGE do DTO DadosListagemMedico automaticamente*/
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
-/*Não vamos retornar uma List<Medico>, pois ele vai retornar todos os atributos e nossa REGRA de negocio quer somente
-* alguns atributos listados, então vamos criar um DTO para retornar os dados corretos, mas precisamos converter Medico
-* para o nosso DTO DadosListagemMedico, criando um construtor no DTO que recebe MEdico como parametro.*/
-    /*Meu map que vai fazer a conversão de Médico para DadosListagemMedico, map(DadosListagemMedico::new) passando
-    * e chamando o construtor no nosso DTO(criar um construtor que recebe do tipo médico)*/
 }
--- src/main/java/med/voll/api/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/medico/MedicoRepository.java
@@ -2,6 +2,5 @@ package med.voll.api.medico;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MedicoRepository extends JpaRepository<Medico, Long> {/*Devido isso aqui, que o nossos métodos JPA
-vai sempre receber um Medico, pois foi declarado aqui*/
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
 }
